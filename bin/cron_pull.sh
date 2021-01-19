cd0b14a2c9e34563d7d723c4cb5ac52de35a59c3
Roberto Polverelli Monti <rpolverelli@gmail.com>
2021-01-19 12:20:51 +0100

    quoting some variables

--- bin/cron_pull.sh
+++ bin/cron_pull.sh
@@ -1,29 +1,31 @@
 #!/usr/bin/env bash
 
+WORKDIR="$HOME/website"
+
 __timestamp() {
 printf "[%s] >>> %s\n" "$(date '+%F %T')" "$1"
 }
 
-LOGFILE="/tmp/$(basename -s '.sh' $0).log"
-LOCKFILE="${LOGFILE/.log/.lock}"
+LOGFILE="/tmp/$(basename -s '.sh' "$0").log"
+LOCKFILE="${LOGFILE%.*}.lock"
 
-if [ -e "$LOCKFILE" ] && kill -0 $(cat "$LOCKFILE"); then
-    __timestamp "Already running." >> $LOGFILE
+if [ -e "$LOCKFILE" ] && kill -0 "$(cat "$LOCKFILE")"; then
+    __timestamp "Already running." >> "$LOGFILE"
     exit 1
 fi
 
-trap "rm -f $LOCKFILE; exit" INT TERM EXIT
+trap "rm -f \"$LOCKFILE\"; exit" INT TERM EXIT
 echo $$ > "$LOCKFILE"
 
-cd $HOME/website || exit 1
+cd "$WORKDIR" || exit 1
 
 git remote update > /dev/null 2>&1
 LOCAL=$(git rev-parse @{0})
 REMOTE=$(git rev-parse @{u})
 
-if [ $LOCAL != $REMOTE ]; then
+if [ "$LOCAL" != "$REMOTE" ]; then
     __timestamp BEGIN
     git reset --hard HEAD &&
         git pull
     __timestamp END
-fi >> $LOGFILE 2>&1
+fi >> "$LOGFILE" 2>&1
